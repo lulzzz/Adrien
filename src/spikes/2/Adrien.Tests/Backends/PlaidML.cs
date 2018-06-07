b5b7473d5ff07a91c622ca908aa23c75f77f13b7
allisterb <allister.beharry@gmail.com>
2018-06-07 14:30:47 -0400

    Added PlaidML backend unit tests.

---
+++ src/spikes/2/Adrien.Tests/Backends/PlaidML.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xunit;
+
+using Adrien.Backend.PlaidML;
+using Adrien.Backend.PlaidML.Bindings;
+
+namespace Adrien.Tests
+{
+    public class PlaidMLBackendTests
+    {
+        [Fact]
+        public void CanGetVersion()
+        {
+            string s = plaidml.PlaidmlGetVersion();
+            Assert.False(string.IsNullOrEmpty(plaidml.PlaidmlGetVersion()));
+        }
+    }
+}
