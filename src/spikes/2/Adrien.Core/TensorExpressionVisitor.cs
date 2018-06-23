5915d6ca2d121e2aa566df4126e62bcf1b5ae485
allisterb <allister.beharry@gmail.com>
2018-06-23 13:11:47 -0400

    Use template to generate Tensor indexers.

--- src/spikes/2/Adrien.Core/TensorExpressionVisitor.cs
+++ src/spikes/2/Adrien.Core/TensorExpressionVisitor.cs
@@ -26,5 +26,7 @@ namespace Adrien
         {
             return base.VisitConstant(node);
         }
+
+        
     }
 }
---
+++ src/spikes/2/Adrien.Tests/ExpressionVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using System.Linq.Expressions;
+using Xunit;
+
+using Adrien.Notation;
+
+namespace Adrien.Tests
+{
+    public class ExpressionVisitorTests
+    {
+        [Fact]
+        public void CanGetVariables()
+        {
+
+            var A = Tensor.TwoD("A", (4, 3), "a", out Index a, out Index b);
+            var B = Tensor.TwoD("B", (6, 7));
+            var C = Tensor.TwoD("C", (8, 9));
+            TensorExpression te = C[a, b] = B[a, b] * C[b, a];
+            TensorExpressionVisitor v = new TensorExpressionVisitor(te);
+            v.Visit(te);
+        }
+    }
+}
