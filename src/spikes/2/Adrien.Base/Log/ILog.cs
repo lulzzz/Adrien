4b97ad6c02f0d5211c77f3d5aee2c6d18fc172af
allisterb <allister.beharry@gmail.com>
2018-08-05 21:09:15 -0400

    ILog.cs excluded by mistake.

---
+++ src/spikes/2/Adrien.Base/Log/ILog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Adrien
+{
+    public interface ILog
+    {
+        void Info(string messageTemplate, params object[] propertyValues);
+
+        void Debug(string messageTemplate, params object[] propertyValues);
+
+        void Error(string messageTemplate, params object[] propertyValues);
+
+        void Error(Exception e, string messageTemplate, params object[] propertyValues);
+
+        void Verbose(string messageTemplate, params object[] propertyValues);
+
+        void Warn(string messageTemplate, params object[] propertyValues);
+    }
+}
