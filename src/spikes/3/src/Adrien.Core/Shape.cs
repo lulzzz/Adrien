6d9dc7e48d8528d1f79b74a00ca7f79e5ccadad0
Joannes Vermorel <joannes.vermorel@lokad.com>
2018-08-29 21:13:50 +0200

    Gather ElementKind and Shape which work together

--- src/spikes/3/src/Adrien.Core/Shape.cs
+++ src/spikes/3/src/Adrien.Core/Shape.cs
@@ -3,6 +3,19 @@
 namespace Adrien.Core
 {
     /// <summary>
+    /// Numeric type of a shape.
+    /// </summary>
+    public enum ElementKind
+    {
+        Int16,
+        Int32,
+        Int64,
+        Float16,
+        Float32,
+        Float64
+    }
+
+    /// <summary>
     /// The shape is the geometric characterization of variables,
     /// symbols and tensors.
     /// </summary>
